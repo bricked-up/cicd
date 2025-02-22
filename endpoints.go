541cef4eeb20c521f4f28e54f46a59f3df992478
Rumen <rumenmitov@protonmail.com>
2025-02-23 00:32:41 +0100

    fix: decoding body with json

--- endpoints.go
+++ endpoints.go
@@ -3,26 +3,40 @@ package main
 import (
     "log"
     "net/http"
+    "encoding/json"
 )
+
+
+type WebHookRequest struct {
+    Ref string `json:"Ref"`
+}
+
 
 func testWebHook(w http.ResponseWriter, r *http.Request) {
     githubEvent := r.Header.Get("x-github-event")
-
-    err := r.ParseForm()
-    if err != nil {
-        log.Println("Invalid HTTP body data.")
-        return
-    }
 
     if githubEvent != "push" {
         log.Println("Recieved an unrecognized Github event:", githubEvent)
         return
     }
 
-    branch := r.PostFormValue("ref")
+    var body []byte
+    var webhook WebHookRequest
 
-    if branch != "refs/heads/master" {
-        log.Println("Push event is not on the desired branch: ", branch)
+    _, err := r.Body.Read(body)
+    if err != nil {
+        log.Println("Could not ready body data.")
+        return
+    }
+
+    err = json.Unmarshal(body, &webhook)
+    if err != nil {
+        log.Println("Could not parse JSON!")
+        return
+    }
+
+    if webhook.Ref != "refs/heads/master" {
+        log.Println("Push event is not on the desired branch: ", webhook.Ref)
         return
     }
 
