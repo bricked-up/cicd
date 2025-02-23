9b369aa34ae8a493795e9ba4bed892fb9ca13f10
Rumen Mitov <108842741+rumenmitov@users.noreply.github.com>
2025-02-23 01:43:12 +0100

    rm unneeded types

--- types.go
+++ types.go
@@ -5,9 +5,3 @@ package main
 type WebHookRequest struct {
     Ref string `json:"Ref"` // Ref refers to the repository branch.
 }
-
-const (
-    ServerOk = 200 // ServerOk is for when the request was processed successfully.
-    InternalServerError = 500 // InternalServerError is for when deployment failed.
-)
-
